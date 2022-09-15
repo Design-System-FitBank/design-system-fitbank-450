e3dde8683d6be6b842c106875469521287fe6b18
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-09-15 14:38:28 -0300

    fix: correção de depedencia do storybook

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   module: {
@@ -21,5 +22,11 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html'
+    })
+  ]
 }
