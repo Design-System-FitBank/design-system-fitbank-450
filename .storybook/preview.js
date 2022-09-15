3b6db45cbfa9a807e57de3ca101e0f2104ce189d
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-09-15 14:13:23 -0300

    chore: adição do storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import {Theme, ThemeProvider} from '../src/theme'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={Theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
