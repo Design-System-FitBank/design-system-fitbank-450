ed347f1c92d7b090a078902890767cd49bb6633e
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-09-15 14:10:53 -0300

    chore: configurações iniciais

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+    {
+        test: /\.js(x)?$/,
+        loader: require.resolve('babel-loader'),
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "@babel/preset-react",
+            "@babel/preset-typescript",
+            "@babel/preset-env"
+          ]
+        }
+      },
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
