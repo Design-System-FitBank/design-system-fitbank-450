723c110f2a480352fa05796884fc002f8deff32f
fit-cledson-leite <109738336+fit-cledson-leite@users.noreply.github.com>
2022-09-19 15:31:09 -0300

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,5 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
   "framework": "@storybook/react"
 }
