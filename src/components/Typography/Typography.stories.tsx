a8b16be6811db15eb39ca14b9257d5749461320d
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-20 17:29:07 -0300

    feat: criando a estrutura basica do componente

---
+++ src/components/Typography/Typography.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Typography } from '.'
+
+export default {
+  title: 'Typography',
+  component: Typography,
+  argTypes: {}
+} as ComponentMeta<typeof Typography>
+
+const Template: ComponentStory<typeof Typography> = args => <Typography {...args} />
+
+export const Headline = Template.bind({})
+Headline.args = {
+  label: 'Headline'
+}
+
+export const Subtitle = Template.bind({})
+
+export const Body = Template.bind({})
+
+export const Button = Template.bind({})
