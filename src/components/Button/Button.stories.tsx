588e1f42d068f9b576f5f2f6c39a3b8e03f54515
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-09-21 12:57:53 -0300

    chore: implementação do storybook atualizado e adição do faker-js para testes

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,25 +1,25 @@
-// import React from 'react';
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-// import {Button} from '.'
+import {Button} from '.'
 
-// export default {
-//   title: 'Button',
-//   component: Button,
-//   argTypes: {
-//   },
-// } as ComponentMeta<typeof Button>;
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+  },
+} as ComponentMeta<typeof Button>;
 
-// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   label: 'Primary',
-//   onClick: () => {}
-// };
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Primary',
+  onClick: () => {}
+};
 
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   secondary: true,
-//   label: 'Secondary',
-// };
+export const Secondary = Template.bind({});
+Secondary.args = {
+  secondary: true,
+  label: 'Secondary',
+};
