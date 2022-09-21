2e4d0e2170c08be5b0fadce2d87798e649f3b7f3
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-09-21 11:14:33 -0300

    chore: desaclopamento do cypress e storybook

--- src/components/Button/Button.cy.tsx
+++ src/components/Button/Button.cy.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { composeStories } from '@storybook/testing-react'
 import { mount } from 'cypress/react18'
 
-import * as stories from './Button.stories'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
+import { Button } from '.'
 
-const { Primary, Secondary } = composeStories(stories)
 
 describe('Button', () => {
   it('Dever mostrar o botão default em caso de primary', () => {
+    const onClickStub = cy.stub()
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <Primary />
+        <Button label='Teste' onClick={onClickStub} />
       </ThemeDSProvider>
     )
     cy.get('.sc-bczRLJ').should('have.css', 'background-color', 'rgb(3, 168, 124)' )
