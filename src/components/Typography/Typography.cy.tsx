79c7bf3dbe2d134204335be069e2b561de78127d
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-21 17:49:51 -0300

    test: ajustes na chamada do teste cy

--- src/components/Typography/Typography.cy.tsx
+++ src/components/Typography/Typography.cy.tsx
@@ -1,35 +1,20 @@
 import React from 'react'
-import { composeStories } from '@storybook/testing-react'
+
 import { mount } from 'cypress/react18'
-
-import * as stories from './Typography.stories'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
-
-const { Headline, Subtitle, Body, Button } = composeStories(stories)
+import { H1 } from './Typography.stories'
 
 describe('Typography', () => {
   it('O Headline deve conter a fonte Roboto, o peso da fonte BOLD e o tamanho de 56px', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <Headline />
+        <H1 type='H1' label='testH1' />
       </ThemeDSProvider>
     )
-    cy.get('.sc-bczRLJ')
+    cy.get('[data-testid = h1]')
       .should('have.css', 'font-family', 'Roboto')
       .and('have.css', 'font-weight', '700')
-      .and('have.css', 'font-size', '10px')
-  }),
-    it('O Subtitle deve conter a fonte-family Roboto, o peso da fonte BOLD e o tamanho de 56px', () => {
-      mount(
-        <ThemeDSProvider theme={Theme}>
-          <GlobalStyles />
-          <Subtitle />
-        </ThemeDSProvider>
-      )
-      cy.get('.sc-bczRLJ')
-        .should('have.css', 'font-family', 'Roboto')
-        .and('have.css', 'font-weight', '700')
-        .and('have.css', 'font-size', '10px')
-    })
+      .and('have.css', 'font-size', '32px')
+  })
 })
