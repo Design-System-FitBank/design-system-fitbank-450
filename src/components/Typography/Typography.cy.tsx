cdcc925a67f1ba52bb63335ab6621b970d0586ca
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-22 17:05:41 -0300

    refactor: finalizando o corpo do componente Typography

--- src/components/Typography/Typography.cy.tsx
+++ src/components/Typography/Typography.cy.tsx
@@ -5,18 +5,154 @@ import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { Typography } from '.'
 
 describe('Typography', () => {
-  it('O Headline deve conter a fonte Roboto, o peso da fonte BOLD e o tamanho de 56px', () => {
-    mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Typography type='H1' label='Headline H1' />
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testid = typography]')
-      .get('[data-testid= "h1Test"]')
-      .should('have.css', 'font-family', 'Roboto')
-      .and('have.css', 'font-weight', '700')
-      .and('have.css', 'font-size', '56px')
-      .and('have.css', 'line-height', '64px')
+  const types = [
+    'H1',
+    'H2',
+    'H3',
+    'H4',
+    'H5',
+    'H6',
+    'Subtitle',
+    'Body',
+    'BodySmall',
+    'BodyBold',
+    'BodyLarge',
+    'Button',
+    'ButtonSmall',
+    'ButtonLarge',
+    'Caption'
+  ]
+
+  const getValues = (types: string) => {
+    var value
+
+    switch (types) {
+      case 'H1':
+        return (value = {
+          label: 'Headline H1',
+          fontSize: '56px',
+          lineHeight: '64px',
+          fontWeight: '700'
+        })
+      case 'H2':
+        return (value = {
+          label: 'Headline H2',
+          fontSize: '48px',
+          lineHeight: '56px',
+          fontWeight: '700'
+        })
+      case 'H3':
+        return (value = {
+          label: 'Headline H3',
+          fontSize: '40px',
+          lineHeight: '48px',
+          fontWeight: '700'
+        })
+      case 'H4':
+        return (value = {
+          label: 'Headline H4',
+          fontSize: '32px',
+          lineHeight: '40px',
+          fontWeight: '700'
+        })
+      case 'H5':
+        return (value = {
+          label: 'Headline H5',
+          fontSize: '24px',
+          lineHeight: '32px',
+          fontWeight: '700'
+        })
+      case 'H6':
+        return (value = {
+          label: 'Headline H6',
+          fontSize: '20px',
+          lineHeight: '24px',
+          fontWeight: '700'
+        })
+
+      case 'Subtitle':
+        return (value = {
+          label: 'Subtitle',
+          fontSize: '16px',
+          lineHeight: 'normal',
+          fontWeight: '500'
+        })
+      case 'Body':
+        return (value = {
+          label: 'Body',
+          fontSize: '16px',
+          lineHeight: '24px',
+          fontWeight: '400'
+        })
+      case 'BodySmall':
+        return (value = {
+          label: 'BodySmall',
+          fontSize: '14px',
+          lineHeight: '20px',
+          fontWeight: '400'
+        })
+      case 'BodyLarge':
+        return (value = {
+          label: 'BodyLarge',
+          fontSize: '18px',
+          lineHeight: '28px',
+          fontWeight: '400'
+        })
+      case 'BodyBold':
+        return (value = {
+          label: 'BodyBold',
+          fontSize: '16px',
+          lineHeight: '24px',
+          fontWeight: '700'
+        })
+      case 'Button':
+        return (value = {
+          label: 'Button',
+          fontSize: '16px',
+          lineHeight: '20px',
+          fontWeight: '700'
+        })
+      case 'ButtonSmall':
+        return (value = {
+          label: 'ButtonSmall',
+          fontSize: '14px',
+          lineHeight: '18px',
+          fontWeight: '700'
+        })
+      case 'ButtonLarge':
+        return (value = {
+          label: 'ButtonSmall',
+          fontSize: '18px',
+          lineHeight: '24px',
+          fontWeight: '700'
+        })
+      case 'Caption':
+        return (value = {
+          label: 'Caption',
+          fontSize: '12px',
+          lineHeight: '16px',
+          fontWeight: '400'
+        })
+    }
+  }
+
+  types.forEach(item => {
+    const value = getValues(item as any)
+
+    it(`Deve conter o tipo ${item}, com a fonte Reboto, o peso ${value?.fontWeight},
+     o tamanho de ${value?.fontSize} e a altura da linha ${value?.lineHeight}`, () => {
+      mount(
+        <ThemeDSProvider theme={Theme}>
+          <GlobalStyles />
+          <Typography type={item as any} label={value?.label as any} />
+        </ThemeDSProvider>
+      )
+      cy.get('[data-testid = "typography"]')
+        .get(`[data-testid = '${item}']`)
+        .should('have.css', 'font-family', 'Roboto')
+        .and('have.css', 'font-weight', `${value?.fontWeight}`)
+        .and('have.css', 'font-size', `${value?.fontSize}`)
+        .and('have.css', 'line-height', `${value?.lineHeight}`)
+    })
   })
 })
--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 import * as Style from './styles'
 interface TypographyProps {
-  type: 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6' | 'Subtitle' | 'BodySmall' | 'BodyBold' | 'BodyLarge' | 'Caption'
+  type:
+    | 'H1'
+    | 'H2'
+    | 'H3'
+    | 'H4'
+    | 'H5'
+    | 'H6'
+    | 'Subtitle'
+    | 'Body'
+    | 'BodySmall'
+    | 'BodyBold'
+    | 'BodyLarge'
+    | 'Button'
+    | 'ButtonSmall'
+    | 'ButtonLarge'
+    | 'Caption'
   label: string
 }
 
@@ -10,25 +25,52 @@ export const Typography = ({ type = 'H1', label }: TypographyProps) => {
   const component = (type: string) => {
     switch (type) {
       case 'H1':
-        return <Style.H1 data-testid='h1Test'>{label}</Style.H1>
+        return <Style.H1 data-testid='H1'>{label}</Style.H1>
 
       case 'H2':
-        return <Style.H2>{label}</Style.H2>
+        return <Style.H2 data-testid='H2'>{label}</Style.H2>
 
       case 'H3':
-        return <Style.H3>{label}</Style.H3>
+        return <Style.H3 data-testid='H3'>{label}</Style.H3>
 
       case 'H4':
-        return <Style.H4>{label}</Style.H4>
+        return <Style.H4 data-testid='H4'>{label}</Style.H4>
 
       case 'H5':
-        return <Style.H5>{label}</Style.H5>
+        return <Style.H5 data-testid='H5'>{label}</Style.H5>
 
       case 'H6':
-        return <Style.H6>{label}</Style.H6>
+        return <Style.H6 data-testid='H6'>{label}</Style.H6>
+
+      case 'Subtitle':
+        return <Style.Subtitle data-testid='Subtitle'>{label}</Style.Subtitle>
+
+      case 'Body':
+        return <Style.Body data-testid='Body'>{label}</Style.Body>
+
+      case 'BodySmall':
+        return <Style.BodySmall data-testid='BodySmall'>{label}</Style.BodySmall>
+
+      case 'BodyBold':
+        return <Style.BodyBold data-testid='BodyBold'>{label}</Style.BodyBold>
+
+      case 'BodyLarge':
+        return <Style.BodyLarge data-testid='BodyLarge'>{label}</Style.BodyLarge>
+
+      case 'Button':
+        return <Style.Button data-testid='Button'>{label}</Style.Button>
+
+      case 'ButtonSmall':
+        return <Style.ButtonSmall data-testid='ButtonSmall'>{label}</Style.ButtonSmall>
+
+      case 'ButtonLarge':
+        return <Style.ButtonLarge data-testid='ButtonLarge'>{label}</Style.ButtonLarge>
+
+      case 'Caption':
+        return <Style.Caption data-testid='Caption'>{label}</Style.Caption>
 
       default:
-        return
+        return <div>Type not found!</div>
     }
   }
 
