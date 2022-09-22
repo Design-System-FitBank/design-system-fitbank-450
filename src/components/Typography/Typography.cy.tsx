f63c4f59c89e1668484ae7ecf1d0379e0c563983
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-22 10:51:30 -0300

    test: implementação do teste h1 do componente typography

--- src/components/Typography/Typography.cy.tsx
+++ src/components/Typography/Typography.cy.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 import { mount } from 'cypress/react18'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
-import { H1 } from './Typography.stories'
+import { Typography } from '.'
 
 describe('Typography', () => {
   it('O Headline deve conter a fonte Roboto, o peso da fonte BOLD e o tamanho de 56px', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <H1 type='H1' label='testH1' />
+        <Typography type='H1' label='Headline H1' />
       </ThemeDSProvider>
     )
-    cy.get('[data-testid = h1]')
+    cy.get('[data-testid = typography]')
+      .get('[data-testid= "h1Test"]')
       .should('have.css', 'font-family', 'Roboto')
       .and('have.css', 'font-weight', '700')
-      .and('have.css', 'font-size', '32px')
+      .and('have.css', 'font-size', '56px')
+      .and('have.css', 'line-height', '64px')
   })
 })
