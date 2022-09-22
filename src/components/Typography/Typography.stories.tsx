514daeaffcfdd9c1157bec264069af95ec709136
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-22 17:06:47 -0300

    test: implementando o teste com o forEach

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -18,7 +18,25 @@ Headline.args = {
 }
 
 export const Subtitle = Template.bind({})
+Subtitle.args = {
+  type: 'Subtitle',
+  label: 'Subtitle'
+}
 
 export const Body = Template.bind({})
+Body.args = {
+  type: 'Body',
+  label: 'Body'
+}
 
 export const Button = Template.bind({})
+Button.args = {
+  type: 'Button',
+  label: 'Button'
+}
+
+export const Caption = Template.bind({})
+Caption.args = {
+  type: 'Caption',
+  label: 'Caption'
+}
