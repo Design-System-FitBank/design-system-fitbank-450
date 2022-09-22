46d53175491beb5b31a8e4755394ff246b5ea168
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-09-22 17:59:44 -0300

    feat: implementação do estado active, desabled e hover do componente Button Default

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,21 +1,35 @@
 import styled from 'styled-components'
 
-export const Container = styled.button`
+type ButtonProps = {
+  disabled?: boolean
+}
+
+export const Container = styled.button<ButtonProps>`
   width: 210px;
   height: 48px;
   padding: 8px 24px;
   gap: 8px;
-  background-color: #323751;
-  border: 1px solid #323751;
+  background-color: ${({ disabled }) => (disabled ? '#ffffff' : '#323751')};
+  border: 1px solid ${({ disabled }) => (disabled ? '#c4c4c4' : '#323751')};
   border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
 
-  color: #fff;
+  color: ${({ disabled }) => (disabled ? '#c4c4c4' : '#ffffff')};
   font-weight: 500;
   font-size: 16px;
   line-height: 19px;
+
+  :hover {
+    color: ${({ disabled }) => (disabled ? '#c4c4c4' : '#fcd669')};
+  }
+
+  :active {
+    background-color: #fff;
+    color: #323751;
+    border: 1px solid #fcd669;
+  }
 `
 export const Icon = styled.div`
   width: 32px;
