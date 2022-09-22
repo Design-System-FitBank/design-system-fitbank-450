278b3d66f53da428f31f4d0ccbba97c6a3a48d2f
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-09-22 14:05:39 -0300

    feat: implementação do estilo default

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Home } from './img-test'
 import * as Styled from './styles'
 
 interface ButtonProps {
   label: string
-  size: 'small' | 'large'
   onClick: () => void
 }
 
-export const Button: React.FC<ButtonProps> = ({ label, size, onClick }) => {
+interface IconProps {}
+
+export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
   return (
-    <Styled.Container data-testeid='button' size={size} onClick={onClick}>
+    <Styled.Container data-testeid='button' onClick={onClick}>
+      <Styled.Icon data-testeid='iconPrefix'>
+        <Home />
+      </Styled.Icon>
       {label}
+      <Styled.Icon data-testeid='iconSuffix'>
+        <Home />
+      </Styled.Icon>
     </Styled.Container>
   )
 }
