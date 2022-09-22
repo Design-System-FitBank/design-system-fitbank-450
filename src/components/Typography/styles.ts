9738feb7d1388c64c5f3ae34c5add2cb89d48c43
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-22 17:05:41 -0300

    refactor: finalizando o corpo do componente Typography

--- src/components/Typography/styles.ts
+++ src/components/Typography/styles.ts
@@ -7,20 +7,75 @@ export const H1 = styled.h1`
 
 export const H2 = styled.h2`
   font-size: 48px;
+  line-height: 56px;
 `
 
 export const H3 = styled.h3`
   font-size: 40px;
+  line-height: 48px;
 `
 
 export const H4 = styled.h4`
   font-size: 32px;
+  line-height: 40px;
 `
 
 export const H5 = styled.h5`
   font-size: 24px;
+  line-height: 32px;
 `
 
 export const H6 = styled.h6`
   font-size: 20px;
+  line-height: 24px;
+`
+
+export const Subtitle = styled.sub`
+  font-size: 16px;
+  line-height: auto;
+  font-weight: 500;
+`
+
+export const Body = styled.body`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+`
+
+export const BodySmall = styled.body`
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+`
+
+export const BodyBold = styled.body`
+  font-size: 16px;
+  line-height: 24px;
+`
+
+export const BodyLarge = styled.body`
+  font-size: 18px;
+  line-height: 28px;
+  font-weight: 400;
+`
+
+export const Button = styled.p`
+  font-size: 16px;
+  line-height: 20px;
+`
+
+export const ButtonLarge = styled.p`
+  font-size: 18px;
+  line-height: 24px;
+`
+
+export const ButtonSmall = styled.p`
+  font-size: 14px;
+  line-height: 18px;
+`
+
+export const Caption = styled.caption`
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 400;
 `
