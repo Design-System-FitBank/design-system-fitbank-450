5e36daa23c40ae1954039f3bd71f20b64f1f7c9c
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-09-23 15:49:16 -0300

    feat: implementação do tamanho small para o botão default

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,24 +2,25 @@ import styled from 'styled-components'
 
 type ButtonProps = {
   disabled?: boolean
+  size?: string
 }
 
 export const Container = styled.button<ButtonProps>`
-  width: 210px;
-  height: 48px;
-  padding: 8px 24px;
-  gap: 7px;
+  width: ${({ size }) => (size === 'small' ? '142px' : '210px')};
+  height: ${({ size }) => (size === 'small' ? '32px' : '48px')};
+  padding: ${({ size }) => (size === 'small' ? '4px 12px' : '8px 24px')};
+  gap: ${({ size }) => (size === 'small' ? '6px' : '7px')};
   background-color: ${({ disabled }) => (disabled ? '#ffffff' : '#323751')};
   border: 1px solid ${({ disabled }) => (disabled ? '#c4c4c4' : '#323751')};
-  border-radius: 10px;
+  border-radius: ${({ size }) => (size === 'small' ? '8px' : '10px')};
   display: flex;
   justify-content: center;
   align-items: center;
 
   color: ${({ disabled }) => (disabled ? '#c4c4c4' : '#ffffff')};
   font-weight: 500;
-  font-size: 16px;
-  line-height: 19px;
+  font-size: ${({ size }) => (size === 'small' ? '12px' : '16px')};
+  line-height: ${({ size }) => (size === 'small' ? '14px' : '19px')};
 
   :hover {
     color: ${({ disabled }) => (disabled ? '#c4c4c4' : '#fcd669')};
@@ -31,13 +32,19 @@ export const Container = styled.button<ButtonProps>`
     border: 1px solid ${({ disabled }) => (disabled ? '#c4c4c4' : '#fcd669')};
   }
 `
-export const Icon = styled.div`
-  width: 32px;
-  height: 32px;
+export const Icon = styled.div<ButtonProps>`
+  width: ${({ size }) => (size === 'small' ? '20px' : '32px')};
+  height: ${({ size }) => (size === 'small' ? '20px' : '32px')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
-export const BoxLabel = styled.div`
-  width: 82px;
-  height: 19px;
+export const BoxLabel = styled.div<ButtonProps>`
+  width: ${({ size }) => (size === 'small' ? '62px' : '82px')};
+  height: ${({ size }) => (size === 'small' ? '14px' : '19px')};
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
