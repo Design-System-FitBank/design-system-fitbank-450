3c3684c026bcef06e30a714933921a47bec1f868
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-09-26 15:45:57 -0300

    test: implementação dos tipos secondary e tertiary do componente Button

--- src/components/Button/Button.cy.tsx
+++ src/components/Button/Button.cy.tsx
@@ -19,25 +19,172 @@ describe('Button Default', () => {
     )
   })
 
-  it('Deve ser o componente Default', () => {
+  it('Deve ser o componente Default quando não for informado o type', () => {
     cy.get('[data-testeid="button"]')
-      .should('be.visible')
-      .and('have.css', 'width', '210px')
-      .and('have.css', 'height', '48px')
-      .and('have.css', 'padding', '8px 24px')
-      .and('have.css', 'gap', '7px')
-      .and('have.css', 'background-color', 'rgb(50, 55, 81)')
-      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
-      .and('have.css', 'border-radius', '10px')
-      .and('have.css', 'display', 'flex')
+      .should('have.css', 'display', 'flex')
       .and('have.css', 'justify-content', 'center')
       .and('have.css', 'align-items', 'center')
       .and('have.text', label)
-      .and('have.css', 'color', 'rgb(255, 255, 255)')
+      .and('have.css', 'cursor', 'pointer')
       .and('have.css', 'font-weight', '500')
+      .and('have.css', 'color', 'rgb(255, 255, 255)')
+      .and('have.css', 'background-color', 'rgb(50, 55, 81)')
+      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
+
+    cy.get('[data-testeid="button"]').realHover().should('have.css', 'color', 'rgb(252, 214, 105)')
+
+    cy.get('[data-testeid="button"]')
+      .realMouseDown()
+      .should('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'border', '1px solid rgb(252, 214, 105)')
+      .and('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 4px 6px 0px')
+  })
+
+  it('Deve ser o componente Default quando for informado o type primary', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={icon} iconRight={false} onClick={cy.stub().as('onClick')} type={'primary'}>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'color', 'rgb(255, 255, 255)')
+      .and('have.css', 'background-color', 'rgb(50, 55, 81)')
+      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
+
+    cy.get('[data-testeid="button"]').realHover().should('have.css', 'color', 'rgb(252, 214, 105)')
+
+    cy.get('[data-testeid="button"]')
+      .realMouseDown()
+      .should('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'border', '1px solid rgb(252, 214, 105)')
+      .and('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 4px 6px 0px')
+  })
+
+  it('Deve ser o componente Default desabled', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={icon} onClick={cy.stub().as('onClick')} disabled>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'color', 'rgb(196, 196, 196)')
+      .and('have.css', 'box-shadow', 'none')
+  })
+
+  it('Deve ser o componente Secondary quando for informado o type secondary', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={icon} iconRight={false} onClick={cy.stub().as('onClick')} type={'secondary'}>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
+      .and('have.css', 'box-shadow', 'none')
+
+    cy.get('[data-testeid="button"]')
+      .realHover()
+      .should('have.css', 'border', '1px solid rgba(0, 0, 0, 0)')
+      .and('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 4px 6px 0px')
+
+    cy.get('[data-testeid="button"]')
+      .realMouseDown()
+      .should('have.css', 'border', '1px solid rgb(252, 214, 105)')
+      .and('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 4px 6px 0px')
+  })
+
+  it('Deve ser o componente Secondary desabled', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={icon} onClick={cy.stub().as('onClick')} disabled type={'secondary'}>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'color', 'rgb(196, 196, 196)')
+      .and('have.css', 'box-shadow', 'none')
+  })
+
+  it('Deve ser o componente Tertiary quando for informado o type tertiary', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={icon} iconRight={false} onClick={cy.stub().as('onClick')} type={'tertiary'}>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+
+    cy.get('[data-testeid="button"]')
+      .realHover()
+      .should('have.css', 'color', 'rgb(95, 130, 166)')
+      .and('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+  })
+
+  it('Deve ser o componente Secondary desabled', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={icon} onClick={cy.stub().as('onClick')} disabled type={'tertiary'}>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'color', 'rgb(196, 196, 196)')
+      .and('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+  })
+
+  it('Deve ser o componente button com tamanho default', () => {
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'width', '210px')
+      .and('have.css', 'height', '48px')
       .and('have.css', 'font-size', '16px')
       .and('have.css', 'line-height', '19px')
-      .and('have.css', 'cursor', 'pointer')
+      .and('have.css', 'border-radius', '10px')
+      .and('have.css', 'padding', '8px 24px')
+      .and('have.css', 'gap', '7px')
+  })
+
+  it('Deve ser o componente button com tamanho large', () => {
+    const size = 'large'
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Button icon={<Home width={20} height={20} />} iconRight={false} size={size} onClick={cy.stub().as('onClick')}>
+          {label}
+        </Button>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testeid="button"]')
+      .should('have.css', 'width', '210px')
+      .and('have.css', 'height', '48px')
+      .and('have.css', 'font-size', '16px')
+      .and('have.css', 'line-height', '19px')
+      .and('have.css', 'border-radius', '10px')
+      .and('have.css', 'padding', '8px 24px')
+      .and('have.css', 'gap', '7px')
   })
 
   it('Deve ser o componente Default com tamanho small', () => {
@@ -53,6 +200,9 @@ describe('Button Default', () => {
     cy.get('[data-testeid="button"]')
       .should('have.css', 'width', '142px')
       .and('have.css', 'height', '32px')
+      .and('have.css', 'font-size', '12px')
+      .and('have.css', 'line-height', '14px')
+      .and('have.css', 'border-radius', '8px')
       .and('have.css', 'padding', '4px 12px')
   })
 
@@ -87,33 +237,6 @@ describe('Button Default', () => {
     )
     cy.get('[data-testeid="iconPrefix"]').should('be.empty')
     cy.get('[data-testeid="iconSuffix"]').should('not.be.empty')
-  })
-
-  it('Deve ser o componente Default no estado hover', () => {
-    cy.get('[data-testeid="button"]').realHover().should('have.css', 'color', 'rgb(252, 214, 105)')
-  })
-
-  it('Deve ser o componente Default active', () => {
-    cy.get('[data-testeid="button"]')
-      .realMouseDown()
-      .should('have.css', 'color', 'rgb(50, 55, 81)')
-      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
-      .and('have.css', 'border', '1px solid rgb(252, 214, 105)')
-  })
-
-  it('Deve ser o componente Default desabled', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Button icon={icon} onClick={cy.stub().as('onClick')} disabled>
-          {label}
-        </Button>
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testeid="button"]')
-      .should('have.css', 'background-color', 'rgb(255, 255, 255)')
-      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
-      .and('have.css', 'color', 'rgb(196, 196, 196)')
   })
 
   it('Deve chamar uma função ao clicar', () => {
