4755e85409cfd591939c0dc9466b3067ad69aeae
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-09-26 16:40:53 -0300

    test: testes do componente typography

--- src/components/Typography/Typography.cy.tsx
+++ src/components/Typography/Typography.cy.tsx
@@ -1,154 +1,156 @@
 import React from 'react'
 
+import { faker } from '@faker-js/faker'
+
 import { mount } from 'cypress/react18'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { Typography } from '.'
 
 describe('Typography', () => {
-  const types = [
-    'H1',
-    'H2',
-    'H3',
-    'H4',
-    'H5',
-    'H6',
-    'Subtitle',
-    'Body',
-    'BodySmall',
-    'BodyBold',
-    'BodyLarge',
-    'Button',
-    'ButtonSmall',
-    'ButtonLarge',
-    'Caption'
+  /**
+   *  Default test
+   */
+  it('Deve conter a variante h1, com a fonte Roboto, o peso 700', () => {
+    const txt = faker.lorem.sentences(3)
+
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Typography variant='h1'>{txt}</Typography>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid = "typography"]')
+      .get(`[data-testid = "h1"]`)
+      .should('have.css', 'font-family', 'Roboto')
+      .and('have.css', 'font-weight', '700')
+      .and('have.css', 'font-size', '56px')
+      .and('have.css', 'line-height', '64px')
+  })
+
+  const variant = [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'subtitle',
+    'body',
+    'bodySmall',
+    'bodyBold',
+    'bodyLarge',
+    'caption'
   ]
 
-  const getValues = (types: string) => {
-    var value
-
-    switch (types) {
-      case 'H1':
-        return (value = {
-          label: 'Headline H1',
+  const getValues = (variant: string) => {
+    switch (variant) {
+      case 'h1':
+        return {
+          type: 'headline',
           fontSize: '56px',
           lineHeight: '64px',
           fontWeight: '700'
-        })
-      case 'H2':
-        return (value = {
-          label: 'Headline H2',
+        }
+      case 'h2':
+        return {
+          type: 'headline',
           fontSize: '48px',
           lineHeight: '56px',
           fontWeight: '700'
-        })
-      case 'H3':
-        return (value = {
-          label: 'Headline H3',
+        }
+      case 'h3':
+        return {
+          type: 'headline',
           fontSize: '40px',
           lineHeight: '48px',
           fontWeight: '700'
-        })
-      case 'H4':
-        return (value = {
-          label: 'Headline H4',
+        }
+      case 'h4':
+        return {
+          type: 'headline',
           fontSize: '32px',
           lineHeight: '40px',
           fontWeight: '700'
-        })
-      case 'H5':
-        return (value = {
-          label: 'Headline H5',
+        }
+      case 'h5':
+        return {
+          type: 'headline',
           fontSize: '24px',
           lineHeight: '32px',
           fontWeight: '700'
-        })
-      case 'H6':
-        return (value = {
-          label: 'Headline H6',
+        }
+      case 'h6':
+        return {
+          type: 'headline',
           fontSize: '20px',
           lineHeight: '24px',
           fontWeight: '700'
-        })
-
-      case 'Subtitle':
-        return (value = {
-          label: 'Subtitle',
+        }
+      case 'subtitle':
+        return {
+          type: 'subtitle',
           fontSize: '16px',
           lineHeight: 'normal',
-          fontWeight: '500'
-        })
-      case 'Body':
-        return (value = {
-          label: 'Body',
+          fontWeight: '500',
+          label: 'Subtitle'
+        }
+
+      case 'body':
+        return {
+          type: 'body',
           fontSize: '16px',
           lineHeight: '24px',
           fontWeight: '400'
-        })
-      case 'BodySmall':
-        return (value = {
-          label: 'BodySmall',
+        }
+      case 'bodySmall':
+        return {
+          type: 'body',
           fontSize: '14px',
           lineHeight: '20px',
           fontWeight: '400'
-        })
-      case 'BodyLarge':
-        return (value = {
-          label: 'BodyLarge',
+        }
+      case 'bodyLarge':
+        return {
+          type: 'body',
           fontSize: '18px',
           lineHeight: '28px',
           fontWeight: '400'
-        })
-      case 'BodyBold':
-        return (value = {
-          label: 'BodyBold',
+        }
+      case 'bodyBold':
+        return {
+          type: 'body',
           fontSize: '16px',
           lineHeight: '24px',
           fontWeight: '700'
-        })
-      case 'Button':
-        return (value = {
-          label: 'Button',
-          fontSize: '16px',
-          lineHeight: '20px',
-          fontWeight: '700'
-        })
-      case 'ButtonSmall':
-        return (value = {
-          label: 'ButtonSmall',
-          fontSize: '14px',
-          lineHeight: '18px',
-          fontWeight: '700'
-        })
-      case 'ButtonLarge':
-        return (value = {
-          label: 'ButtonSmall',
-          fontSize: '18px',
-          lineHeight: '24px',
-          fontWeight: '700'
-        })
-      case 'Caption':
-        return (value = {
-          label: 'Caption',
+        }
+      case 'caption':
+        return {
+          type: 'button',
           fontSize: '12px',
           lineHeight: '16px',
-          fontWeight: '400'
-        })
+          fontWeight: '400',
+          label: 'Caption'
+        }
+      default:
+        'Variant not found'
     }
   }
 
-  types.forEach(item => {
-    const value = getValues(item as any)
+  variant.forEach(item => {
+    const value = getValues(item)
 
-    it(`Deve conter o tipo ${item}, com a fonte Reboto, o peso ${value?.fontWeight},
-     o tamanho de ${value?.fontSize} e a altura da linha ${value?.lineHeight}`, () => {
+    it(`Deve conter a variante ${item}, com a fonte Roboto, o peso ${value?.fontWeight},
+    o tamanho de ${value?.fontSize} e a altura da linha ${value?.lineHeight}`, () => {
+      const txt = faker.lorem.sentences(3)
+
       mount(
         <ThemeDSProvider theme={Theme}>
           <GlobalStyles />
-          <Typography type={item as any} label={value?.label as any} />
+          <Typography variant={item as any}>{txt}</Typography>
         </ThemeDSProvider>
       )
       cy.get('[data-testid = "typography"]')
-        .get(`[data-testid = '${item}']`)
+        .get(`[data-testid = "${item}"]`)
         .should('have.css', 'font-family', 'Roboto')
         .and('have.css', 'font-weight', `${value?.fontWeight}`)
         .and('have.css', 'font-size', `${value?.fontSize}`)
