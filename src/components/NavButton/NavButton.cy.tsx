02e9e0bee88d4dc48727d37e646a42a76b9b42c4
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-10-03 18:53:56 -0300

    test: criação do componente navbutton small com estados hover e active

--- src/components/NavButton/NavButton.cy.tsx
+++ src/components/NavButton/NavButton.cy.tsx
@@ -7,7 +7,7 @@ import { Icon } from '../Icon'
 import { NavButton } from '.'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 
-describe('NavButton', () => {
+describe('NavButton Default', () => {
   const icon = <Icon name='moneyNote' width={38} height={38} />
   const label = faker.lorem.word()
   beforeEach(() => {
@@ -80,5 +80,55 @@ describe('NavButton', () => {
       .should('have.css', 'border', '1px solid rgb(196, 196, 196)')
       .and('have.css', 'color', 'rgb(196, 196, 196)')
       .and('have.css', 'box-shadow', 'none')
+  })
+})
+
+describe('NavButton Small', () => {
+  const icon = <Icon name='moneyNote' width={38} height={38} />
+  const label = faker.lorem.word()
+  beforeEach(() => {
+    cy.wait(100)
+  })
+  afterEach(() => {
+    cy.wait(100)
+  })
+  it('Deve ser o componente Small quando passar o size small', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <NavButton icon={icon} onClick={cy.stub().as('onclick')} size={'small'}>
+          {label}
+        </NavButton>
+      </ThemeDSProvider>
+    )
+      .get('[data-testid="nav-button"]')
+      .should('have.css', 'width', '54px')
+      .and('have.css', 'height', '54px')
+  })
+
+  it('Deve ser o componente Small quando passar o size small no estado Hover', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <NavButton icon={icon} onClick={cy.stub().as('onclick')} size={'small'}>
+          {label}
+        </NavButton>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="nav-button"]').realHover()
+    cy.get('[data-testid="nav-button"]').should('have.css', 'border', '1px solid rgb(196, 196, 196)')
+  })
+
+  it('Deve ser o componente Small quando passar o size small no estado Active', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <NavButton icon={icon} onClick={cy.stub().as('onclick')} size={'small'}>
+          {label}
+        </NavButton>
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="nav-button"]').realMouseDown()
+    cy.get('[data-testid="nav-button"]').should('have.css', 'border', '1px solid rgb(50, 55, 81)')
   })
 })
