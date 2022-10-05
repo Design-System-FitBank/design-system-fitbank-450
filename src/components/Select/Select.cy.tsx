2ec02d69b68cc3e93e0b9ca088c948e93f54c566
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-10-05 09:44:21 -0300

    test: atualizando a implementação dos testes do componente

--- src/components/Select/Select.cy.tsx
+++ src/components/Select/Select.cy.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { faker } from '@faker-js/faker'
+import 'cypress-real-events/support'
 
 import { mount } from 'cypress/react18'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
@@ -8,15 +9,14 @@ import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { Select } from '.'
 
 describe('Select', () => {
+  const placeholder = faker.lorem.word()
+  const title = faker.lorem.word()
+  const opts = [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()]
+
   /**
    *  Default test
    */
-  it('Deve conter titulo, e placeholder', () => {
-    const placeholder = faker.lorem.word()
-    const title = faker.lorem.word()
-
-    const opts = [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()]
-
+  it('Deve conter titulo e placeholder', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
@@ -25,6 +25,38 @@ describe('Select', () => {
     )
     cy.get('[data-testid = "label"]').should('have.text', title)
     cy.get('[data-testid = "input"]').should('have.attr', 'placeholder', placeholder)
-    //.and('have.attr', 'options')
+  })
+
+  it('Deve conter o componente Select default', () => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Select title={title} placeholder={placeholder} options={opts} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid = "container"]')
+      .should('have.css', 'max-width', '320px')
+      .and('have.css', 'padding-top', '12px')
+      .and('have.css', 'padding-right', '12px')
+      .and('have.css', 'padding-bottom', '12px')
+      .and('have.css', 'padding-left', '16px')
+      .and('have.css', 'gap', '8px')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'flex-direction', 'row')
+      .and('have.css', 'align-items', 'center')
+      .and('have.css', 'justify-content', 'space-evenly')
+      .and('have.css', 'border-radius', '6px')
+  })
+
+  it('Deve conter o estado Hover', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Select title={title} placeholder={placeholder} options={opts} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="container"]').realHover()
+    cy.get('[data-testid="container"]').should('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
   })
 })
