0ed53e28e36e68e982a9756e9a0da795983ee3a3
cledson-leite <wesley.morais@fitbank.com.br>
2022-10-07 08:17:00 -0300

    test: componente Input Default

--- src/components/Input/Input.cy.tsx
+++ src/components/Input/Input.cy.tsx
@@ -4,17 +4,50 @@ import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { Input } from '.'
 
 describe('Input component', () => {
-  it('Deve ser informado o titulo e placeholder no componente Default', () => {
-    const titleFake = 'testLabel'
-    const placeholderFake = 'testPlaceholder'
+  const titleFake = 'Input Label'
+  const placeholderFake = 'Placeholder'
 
+  it('Deve ser informado o titulo e placeholder no componente Default', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <Input title={titleFake} placeholder={placeholderFake} />
+        <Input type='text' title={titleFake} placeholder={placeholderFake} />
       </ThemeDSProvider>
     )
     cy.get('[data-testid="label"]').should('have.text', titleFake)
     cy.get('[data-testid="input"]').should('have.attr', 'placeholder', placeholderFake)
+  })
+
+  it('Deve mostrar o componente Input Default com seus respectivos valores de estilização', () => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Input type='text' title={titleFake} placeholder={placeholderFake} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="label"]')
+      .should('have.css', 'max-width', '320px')
+      .and('have.css', 'max-height', '20px')
+      .and('have.css', 'text-transform', 'capitalize')
+      .and('have.css', 'color', 'rgb(50, 55, 81)')
+    cy.get('[data-testid="input"]')
+      .should('have.css', 'width', '320px')
+      .and('have.css', 'height', '48px')
+      .and('have.css', 'border-radius', '6px')
+      .and('have.css', 'padding-top', '12px')
+      .and('have.css', 'padding-right', '16px')
+      .and('have.css', 'padding-bottom', '12px')
+      .and('have.css', 'padding-left', '16px')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+  })
+
+  it('Deve mostrar o componente Input Password', () => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Input type='password' title={titleFake} placeholder={placeholderFake} />
+      </ThemeDSProvider>
+    )
   })
 })
