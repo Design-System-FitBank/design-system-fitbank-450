2c83d1466d078e1375c00eb670a620b4f1145557
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-10-17 17:17:41 -0300

    test: implementação do item do componente input checkbox

--- src/components/Icon/Icon.cy.tsx
+++ src/components/Icon/Icon.cy.tsx
@@ -24,6 +24,7 @@ describe('Icon component', () => {
     'card',
     'cardMulti',
     'changeAccount',
+    'checked',
     'cnh',
     'details',
     'device',
--- src/components/InputCheckbox/InputItem/InputItem.cy.tsx
+++ src/components/InputCheckbox/InputItem/InputItem.cy.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import 'cypress-real-events/support'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../../theme'
 import { InputItem } from '.'
+import { faker } from '@faker-js/faker'
 
 describe('Checkbox', () => {
+  const option = faker.lorem.word()
+  beforeEach(() => {
+    cy.wait(100)
+  })
+  afterEach(() => {
+    cy.wait(100)
+  })
   it('Deve ser o componente checkbox', () => {
     cy.mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <InputItem />
+        <InputItem option={option} onclick={cy.stub().as('onclick')} />
       </ThemeDSProvider>
     )
 
@@ -15,6 +24,75 @@ describe('Checkbox', () => {
       .should('have.css', 'width', '15px')
       .and('have.css', 'height', '15px')
       .and('have.css', 'border-radius', '3px')
-      .and('have.css', 'border', '1px solid rgb(51, 55, 80)')
+      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'display', 'flex')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'align-items', 'center')
+  })
+
+  it('Deve ser o componente checkbox hover', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <InputItem option={option} onclick={cy.stub().as('onclick')} />
+      </ThemeDSProvider>
+    )
+
+    cy.get('[data-testid="checkbox"]').realHover()
+    cy.get('[data-testid="checkbox"]')
+      .should('have.css', 'width', '15px')
+      .and('have.css', 'height', '15px')
+      .and('have.css', 'border-radius', '3px')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'display', 'flex')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'align-items', 'center')
+  })
+
+  it('Deve ser o componente checkbox checkado', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <InputItem checked={true} option={option} onclick={cy.stub().as('onclick')} />
+      </ThemeDSProvider>
+    )
+
+    cy.get('[data-testid="checkbox"]')
+      .should('have.css', 'width', '15px')
+      .and('have.css', 'height', '15px')
+      .and('have.css', 'border-radius', '3px')
+      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgb(50, 55, 81)')
+      .and('have.css', 'color', 'rgb(255, 255, 255)')
+      .and('have.css', 'display', 'flex')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'align-items', 'center')
+
+    cy.get('[data-testid="checkbox-icon"]').should('not.be.empty')
+  })
+
+  it('Deve ser o componente checkbox com label', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <InputItem option={option} onclick={cy.stub().as('onclick')} />
+      </ThemeDSProvider>
+    )
+
+    cy.get('[data-testid="checkbox"]')
+      .should('have.css', 'width', '15px')
+      .and('have.css', 'height', '15px')
+      .and('have.css', 'border-radius', '3px')
+      .and('have.css', 'border', '1px solid rgb(50, 55, 81)')
+      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'display', 'flex')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'align-items', 'center')
+
+    cy.get('[data-testid="typography"]').should('exist').and('have.css', 'color', 'rgb(50, 55, 81)')
+
+    cy.get('[data-testid="input-checkbox"]').should('have.css', 'gap', '26px')
   })
 })
