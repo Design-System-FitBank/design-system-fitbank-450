17df71d09fdd6c9a141f094edb26dc1919905c35
cledson-leite <wesley.morais@fitbank.com.br>
2022-10-18 19:56:21 -0300

    test: conclusão pacial do componente input

--- src/components/Input/Input.cy.tsx
+++ src/components/Input/Input.cy.tsx
@@ -9,7 +9,6 @@ describe('Input component', () => {
   const titleFake = 'Input Label'
   const placeholderFake = 'Placeholder'
   const textFaker = faker.lorem.word()
-  const erroMessage = faker.lorem.sentence()
 
   beforeEach(() => {
     cy.wait(500)
@@ -21,24 +20,26 @@ describe('Input component', () => {
     )
   })
 
-  it('Deve mostrar o componente Default', () => {
+  it('Deve mostrar o componente default', () => {
     cy.get('[data-testid="label"]')
       .should('have.text', titleFake)
       .and('have.css', 'text-transform', 'capitalize')
       .and('have.css', 'color', 'rgb(50, 55, 81)')
       .and('have.css', 'justify-content', 'flex-start')
+      .and('have.css', 'margin-bottom', '4px')
+
     cy.get('[data-testid="input"]')
       .should('have.attr', 'placeholder', placeholderFake)
       .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
-      .and('have.css', 'color', 'rgb(196, 196, 196)')
+      .and('have.css', 'color', 'rgb(50, 55, 81)')
       .and('have.css', 'background-color', 'rgb(255, 255, 255)')
       .and('have.css', 'font-weight', '400')
       .and('have.css', 'font-size', '18px')
       .and('have.css', 'justify-content', 'flex-start')
       .and('have.css', 'padding-left', '16px')
-
-    cy.get('[data-testid="icon"]').get('[data-testid="eyeOpen"]').should('not.exist')
-    cy.get('[data-testid="icon"]').get('[data-testid="eyeClose"]').should('not.exist')
+      .and('have.css', 'width', '320px')
+      .and('have.css', 'height', '48px')
+      .and('have.css', 'border-radius', '6px')
   })
 
   it('Deve mostrar o componente tipo password', () => {
@@ -55,6 +56,7 @@ describe('Input component', () => {
       .and('have.css', 'width', '24px')
       .and('have.css', 'height', '24px')
       .and('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'top', '7px')
 
     cy.get('[data-testid="icon"]').get('[data-testid="eyeOpen"]').should('exist')
     cy.get('[data-testid="icon"]').get('[data-testid="eyeClose"]').should('not.exist')
@@ -83,6 +85,10 @@ describe('Input component', () => {
       .and('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
   })
 
+  it('Deve mostrar componente default preenchido após desfocado', () => {
+    cy.get('[data-testid="input"]').type(textFaker).and('have.css', 'color', 'rgb(50, 55, 81)')
+  })
+
   it('Deve mostrar componente default desabilitado ', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
@@ -95,6 +101,19 @@ describe('Input component', () => {
       .and('have.css', 'background-color', 'rgb(196, 196, 196)')
   })
 
+  it('Deve mostrar componente password desabilitado ', () => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Input type='password' title={titleFake} placeholder={placeholderFake} disabled={true} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="input"]')
+      .should('have.attr', 'placeholder', '••••••••')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'background-color', 'rgb(196, 196, 196)')
+  })
+
   it('Deve mostrar mensagem quando receber mensagem de erro de validação', () => {
     const messageErro = faker.lorem.sentence()
     mount(
@@ -104,7 +123,37 @@ describe('Input component', () => {
       </ThemeDSProvider>
     )
     cy.get('[data-testid="input"]').should('have.css', 'border', '1px solid rgb(234, 62, 62)')
-    cy.get('[data-testid="message"]').should('have.text', messageErro).and('have.css', 'color', 'rgb(234, 62, 62)')
+    cy.get('[data-testid="message"]')
+      .should('have.text', messageErro)
+      .and('have.css', 'color', 'rgb(234, 62, 62)')
+      .and('have.css', 'margin-top', '4px')
+      .and('have.css', 'justify-content', 'flex-start')
+  })
+
+  it('Deve mostrar componente default validado quando a função validadora não retornar mensagem', () => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Input type='text' title={titleFake} placeholder={placeholderFake} disabled={false} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="input"]').type(textFaker)
+    cy.wait(4000)
+    cy.get('[data-testid="input"]').should('have.css', 'border', '1px solid rgb(196, 196, 196)')
+  })
+
+  it('Deve mostrar componente password validado quando a função validadora não retornar mensagem', () => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Input type='password' title={titleFake} placeholder={placeholderFake} disabled={false} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="input"]').type(textFaker)
+    cy.get('[data-testid="icon"]')
+      .get('[data-testid="done"]')
+      .should('exist')
+      .and('have.css', 'color', 'rgb(12, 194, 96)')
   })
 
   // it('Deve mostrar o componente do tipo Default com valores no campo de texto', () => {
