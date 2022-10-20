39c657fecc8c02a2bad8e4bc4db0c5d90963d392
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-10-20 14:40:59 -0300

    test: refatoraçoes de testes desnecessários e implementação do teste do onSelected

--- src/components/Select/Select.cy.tsx
+++ src/components/Select/Select.cy.tsx
@@ -9,7 +9,17 @@ import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { Select } from '.'
 
 describe('Select', () => {
+  const placeholder = faker.lorem.word()
+  const title = faker.lorem.word()
+  const optionsList = [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()]
+
   beforeEach(() => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Select title={title} placeholder={placeholder} options={optionsList} onSelected={cy.stub().as('onSelected')} />
+      </ThemeDSProvider>
+    )
     cy.wait(100)
   })
 
@@ -17,65 +27,22 @@ describe('Select', () => {
     cy.wait(100)
   })
 
-  const placeholder = faker.lorem.word()
-  const title = faker.lorem.word()
-  const opts = [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()]
-
   it('Deve conter titulo, placeholder e uma lista de opções', () => {
-    mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testid = "select"]')
-    cy.get('[data-testid = "label"]').should('have.text', title)
-    cy.get('[data-testid = "input"]')
+    cy.get('[data-testid = "label"]')
+      .should('have.text', title)
+      .and('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'max-width', '320px')
+      .and('have.css', 'max-height', '20px')
     cy.get('[data-testid = "inputContent"]').should('have.attr', 'placeholder', placeholder)
   })
 
-  opts.forEach((text: string) => {
+  optionsList.forEach((text: string) => {
     it(`Deve ser o texto ${text} da lista de Options`, () => {
-      cy.mount(
-        <ThemeDSProvider theme={Theme}>
-          <GlobalStyles />
-          <Select title={title} placeholder={placeholder} options={opts} />
-        </ThemeDSProvider>
-      )
       cy.contains(text).should('exist')
     })
   })
 
-  it('Deve conter o titulo com a cor primaria, com altura de 20px e comprimento 320px', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testid="label"]')
-      .should('have.css', 'color', 'rgb(50, 55, 81)')
-      .and('have.css', 'max-width', '320px')
-      .and('have.css', 'max-height', '20px')
-  })
-
-  it('O valor do input deve começar como vazio', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testid="input"]').should('have.value', '')
-  })
-
   it('Deve conter o componente Select default', () => {
-    mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
     cy.get('[data-testid = "select"]')
     cy.get('[data-testid = "input"]')
       .should('have.css', 'max-width', '320px')
@@ -90,60 +57,37 @@ describe('Select', () => {
       .and('have.css', 'align-items', 'center')
       .and('have.css', 'justify-content', 'space-evenly')
       .and('have.css', 'border-radius', '6px')
-      .and('have.css', 'transition', 'all 0.5s ease 0s')
   })
 
   it('Deve conter o estado Hover com uma aparencia da elevação do componente', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
     cy.get('[data-testid="input"]').realHover()
     cy.get('[data-testid="input"]').should('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
   })
 
   it('Deve ter a animação de rotação no icone de seta quando for apertado o input', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
     cy.get('[data-testid="input"]').click()
     cy.get('[data-testid="arrowDown"]')
       .should('have.css', 'cursor', 'pointer')
       .and('have.css', 'transition', 'all 0s ease 0s')
   })
 
-  it('Deve conter a lista de opções quando clicar no input com no maximo 3 itens. Com a fonte dos itens Roboto e font-size de 16px', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
+  it('Deve conter a lista de opções quando clicar no input com no maximo 3 itens.', () => {
     cy.get('[data-testid="input"]').click()
-    cy.get('[data-testid="modal"]')
-      .should('have.css', 'width', '320px')
-      .and('have.css', 'max-width', '320px')
-    cy.get('[data-testid="modal"]')
-      .should('have.css', 'font-family', 'Roboto')
-      .and('have.css', 'font-size', '16px')
+    cy.get('[data-testid="modal"]').should('have.css', 'width', '320px').and('have.css', 'max-width', '320px')
+    cy.get('[data-testid="modal"]').should('have.css', 'font-family', 'Roboto').and('have.css', 'font-size', '16px')
   })
 
   it('Deve conter o background light no modal. Junto com a barra de scroll quando tiver mais de 3 itens de opção', () => {
-    cy.mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <Select title={title} placeholder={placeholder} options={opts} />
-      </ThemeDSProvider>
-    )
     cy.get('[data-testid="input"]').click()
     cy.get('[data-testid="modal"]')
       .should('have.css', 'width', '320px')
       .and('have.css', 'max-width', '320px')
       .and('have.css', 'overflow-y', 'scroll')
+  })
+
+  it('Deve a chamar a função onSelected', () => {
+    cy.get('[data-testid="input"]').click()
+    cy.get('[data-testid="modal"]').get('[data-testid="item-0"]').click()
+    cy.get('@onSelected').should('have.been.calledWith', optionsList[0])
   })
 })
