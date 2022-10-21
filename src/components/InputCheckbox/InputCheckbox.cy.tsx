63678cf13a566e38178f9694c71f030f5c2aefec
Fit-Marcelle-Tabosa <marcelle.souza@fitbank.com.br>
2022-10-21 17:14:03 -0300

    test: implementação da propriedade direction

--- src/components/InputCheckbox/InputCheckbox.cy.tsx
+++ src/components/InputCheckbox/InputCheckbox.cy.tsx
@@ -6,17 +6,31 @@ import { InputCheckbox } from '.'
 describe('Input Checkbox', () => {
   const title = faker.lorem.words()
   const list = [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()]
-  it('Deve ser o componente Input Checkbox com lista de opções', () => {
+  it('Deve ser o componente Input Checkbox Default', () => {
     cy.mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <InputCheckbox title={title} optionsList={list} />
+        <InputCheckbox title={title} optionsList={list} onSelect={cy.stub().as('onSelect')} />
       </ThemeDSProvider>
     )
     cy.get('[data-testid="input-checkbox"]')
       .should('not.be.empty')
       .and('have.css', 'text-transform', 'capitalize')
       .and('have.css', 'color', 'rgb(50, 55, 81)')
+      .and('have.css', 'display', 'flex')
+      .and('have.css', 'flex-direction', 'column')
+
+    cy.get('[data-testid="input-checkbox-title"]').should('have.text', title)
+  })
+
+  it('Deve ser o componente Input Checkbox Default quando for informado a direction row', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <InputCheckbox title={title} direction={'row'} optionsList={list} onSelect={cy.stub().as('onSelect')} />
+      </ThemeDSProvider>
+    )
+    cy.get('[data-testid="input-checkbox"]').should('not.be.empty').and('have.css', 'flex-direction', 'row')
 
     cy.get('[data-testid="input-checkbox-title"]').should('have.text', title)
   })
