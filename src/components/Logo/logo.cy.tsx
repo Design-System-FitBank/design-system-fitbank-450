56055477a027573777ceae1eceef74339704f8c0
cledson-leite <wesley.morais@fitbank.com.br>
2022-10-25 09:41:23 -0300

    test: retirado os atributos de largura e altura

--- src/components/Logo/logo.cy.tsx
+++ src/components/Logo/logo.cy.tsx
@@ -5,33 +5,21 @@ import { Logo } from '.'
 describe('Logo-FB-default componente', () => {
   it('Deve mostrar o componente logo default com o nome fitbank', () => {
     mount(<Logo name='default' />)
-    cy.get('[data-testid="primary"]')
-      .should('be.visible')
-      .and('have.css', 'width', '110px')
-      .and('have.css', 'height', '110px')
+    cy.get('[data-testid="primary"]').should('be.visible')
   })
 
   it('Deve mostrar o componente logo na forma secundária com o nome fitbank', () => {
     mount(<Logo name='secundary' />)
-    cy.get('[data-testid="secundary"]')
-      .should('be.visible')
-      .and('have.css', 'width', '110px')
-      .and('have.css', 'height', '110px')
+    cy.get('[data-testid="secundary"]').should('be.visible')
   })
 
   it('Deve mostrar componente default small na cor primária', () => {
     mount(<Logo name='smallPrimary' />)
-    cy.get('[data-testid="smallPrimary"]')
-      .should('be.visible')
-      .and('have.css', 'width', '110px')
-      .and('have.css', 'height', '110px')
+    cy.get('[data-testid="smallPrimary"]').should('be.visible')
   })
 
   it('Deve mostrar componente secundário small na cor secundária', () => {
     mount(<Logo name='smallSecundary' />)
-    cy.get('[data-testid="smallSecundary"]')
-      .should('be.visible')
-      .and('have.css', 'width', '110px')
-      .and('have.css', 'height', '110px')
+    cy.get('[data-testid="smallSecundary"]').should('be.visible')
   })
 })
