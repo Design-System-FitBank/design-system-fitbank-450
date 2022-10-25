faf3d6c09874befcac2c9bc99df4c65dd9a9c6d5
cledson-leite <wesley.morais@fitbank.com.br>
2022-10-25 09:40:39 -0300

    refactor: retirado os atributos de largura e altura

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,34 +6,24 @@ import { LogoSmallSecundary } from './logos/LogoSmallSecundary'
 
 interface LogoProps {
   /**
-   *referente à largura do componente.
-   *caso não seja setada o seu valor default é de 110px
-   */
-  width?: number
-  /**
-   * referente à altura do componente.
-   * caso não seja setada o seu valor default é de 110px
-   */
-  heigth?: number
-  /**
    * Variáveis referente aos tipos de logos
    */
   name: 'default' | 'secundary' | 'smallPrimary' | 'smallSecundary'
 }
 
-export const Logo: React.FC<LogoProps> = ({ name, width = 110, heigth = 110 }) => {
+export const Logo: React.FC<LogoProps> = ({ name }) => {
   switch (name) {
     case 'default':
-      return <LogoPrimary data-testid='primary' width={width} height={heigth} />
+      return <LogoPrimary data-testid='primary' />
 
     case 'secundary':
-      return <LogoSecundary data-testid='secundary' width={width} height={heigth} />
+      return <LogoSecundary data-testid='secundary' />
 
     case 'smallPrimary':
-      return <LogoSmallPrimary data-testid='smallPrimary' width={width} height={heigth} />
+      return <LogoSmallPrimary data-testid='smallPrimary' />
 
     case 'smallSecundary':
-      return <LogoSmallSecundary data-testid='smallSecundary' width={width} height={heigth} />
+      return <LogoSmallSecundary data-testid='smallSecundary' />
 
     default:
       return <div></div>
