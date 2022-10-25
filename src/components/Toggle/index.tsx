c090859e0f3b5234e684683427909d51ca84e15b
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-10-25 17:22:22 -0300

    feat: implementação do componente Toggle

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Typography } from '../Typography'
+import { ToggleContainer, Switcher, Container } from './styles'
+
+interface ToggleProps {
+  /**
+   * Titulo opcional do toggle
+   */
+  title?: string
+  /**
+   * Função que retorna o estado do Toggle, sendo Inativo ou Ativo
+   */
+  isActive: (value: boolean) => void
+}
+
+export const Toggle: React.FC<ToggleProps> = ({ title, isActive }) => {
+  const [isToggled, setIsToggled] = useState(false)
+
+  const handleOnToggleSwitched = () => {
+    const activeToggle = !isToggled
+    setIsToggled(activeToggle)
+    isActive(activeToggle)
+  }
+
+  return title ? (
+    <Container data-testid='container'>
+      <ToggleContainer data-testid='toggle' isToggled={isToggled} onClick={() => handleOnToggleSwitched()}>
+        <Switcher data-testid='switcher' isToggled={isToggled} />
+      </ToggleContainer>
+      <Typography data-testid='bodyLarge' variant='bodyLarge'>
+        {title}
+      </Typography>
+    </Container>
+  ) : (
+    <ToggleContainer data-testid='toggle' isToggled={isToggled} onClick={() => handleOnToggleSwitched()}>
+      <Switcher data-testid='switcher' isToggled={isToggled} />
+    </ToggleContainer>
+  )
+}
