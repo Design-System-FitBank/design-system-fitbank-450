ea6b1aa7019cef1c52fdc178a1256649a0eebdaf
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-10-26 17:55:17 -0300

    test: implementação de novos testes no componente, pinToken

--- src/components/PinToken/PinToken.cy.tsx
+++ src/components/PinToken/PinToken.cy.tsx
@@ -5,9 +5,16 @@ import 'cypress-real-events/support'
 
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { PinToken } from '.'
+import { faker } from '@faker-js/faker'
 
 describe('PinToken', () => {
   beforeEach(() => {
+    mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <PinToken onChange={cy.stub().as('onChange')} />
+      </ThemeDSProvider>
+    )
     cy.wait(100)
   })
 
@@ -16,12 +23,6 @@ describe('PinToken', () => {
   })
 
   it('Deve conter um container por volta do PinToken', () => {
-    mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <PinToken onChange={cy.stub().as('onChange')} />
-      </ThemeDSProvider>
-    )
     cy.get('[data-testid = "container"]')
       .should('have.css', 'display', 'flex')
       .and('have.css', 'flex-direction', 'row')
@@ -31,36 +32,11 @@ describe('PinToken', () => {
   })
 
   it('Deve conter o PinToken default', () => {
-    mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <PinToken onChange={cy.stub().as('onChange')} />
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testid = "container"]')
-    cy.get('[data-testid = "pinToken"]')
-      .should('have.css', 'width', '41.9886360168457px')
-      .and('have.css', 'height', '41.9886360168457px')
+    cy.get('[data-testid = "pinToken-0"]')
+      .should('have.css', 'width', '42px')
+      .and('have.css', 'height', '42px')
       .and('have.css', 'background-color', 'rgb(255, 255, 255)')
-      .and('have.css', 'border', '0.994318px solid rgb(196, 196, 196)')
-      .and('have.css', 'border-radius', '6px')
-      .and('have.css', 'text-align', 'center')
-      .and('have.css', 'font-size', '18px')
-      .and('have.css', 'line-height', '24px')
-  })
-  it('Deve conter o PinToken default', () => {
-    mount(
-      <ThemeDSProvider theme={Theme}>
-        <GlobalStyles />
-        <PinToken onChange={cy.stub().as('onChange')} />
-      </ThemeDSProvider>
-    )
-    cy.get('[data-testid = "container"]')
-    cy.get('[data-testid = "pinToken"]')
-      .should('have.css', 'width', '41.9886360168457px')
-      .and('have.css', 'height', '41.9886360168457px')
-      .and('have.css', 'background-color', 'rgb(255, 255, 255)')
-      .and('have.css', 'border', '0.994318px solid rgb(196, 196, 196)')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
       .and('have.css', 'border-radius', '6px')
       .and('have.css', 'text-align', 'center')
       .and('have.css', 'font-size', '18px')
@@ -68,18 +44,56 @@ describe('PinToken', () => {
   })
 
   it('Deve conter o estado Hover com uma aparencia da elevação do componente', () => {
+    cy.get('[data-testid="pinToken-0"]').realHover()
+    cy.get('[data-testid="pinToken-0"]').should('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
+  })
+
+  it('Deve conter o estado Focused quando o pinToken for clicado', () => {
+    cy.get('[data-testid="pinToken-0"]').click()
+    cy.get('[data-testid="pinToken-0"]').should('have.css', 'border', '1px solid rgb(1, 7, 22)')
+  })
+
+  it('Deve conter o estado Error quando o pinToken clicado for digitado um caractere for diferente de um numero', () => {
+    const isNaNChar = faker.random.alpha()
+
+    cy.get('[data-testid="pinToken-0"]')
+      .click()
+      .type(isNaNChar)
+      .should('have.css', 'border', '1px solid rgb(234, 62, 62)')
+  })
+
+  it('Deve conter o estado Filed quando o pinToken clicado for digitado um numero', () => {
+    const validPin = faker.random.alphaNumeric()
+
+    cy.get('[data-testid="pinToken-0"]')
+      .click()
+      .type('1')
+      .should('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'text-align', 'center')
+      .and('have.css', 'font-size', '18px')
+      .and('have.css', 'line-height', '24px')
+  })
+
+  it('Deve conter o estado Disabled', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <PinToken onChange={cy.stub().as('onChange')} />
+        <PinToken onChange={cy.stub().as('onChange')} disabled />
       </ThemeDSProvider>
     )
-    cy.get('[data-testid="pinToken"]').realHover()
-    cy.get('[data-testid="pinToken"]').should('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
+    cy.get('[data-testid = "pinToken-0"]')
+      .and('have.css', 'border', '1px solid rgb(196, 196, 196)')
+      .and('have.css', 'background-color', 'rgb(196, 196, 196)')
   })
 
   // it('Deve a chamar a função onPinChange', () => {
-  //   cy.get('[data-testid="pinToken"]').click()
+  //   cy.get('[data-testid="pinToken-0"]')
+  //     .click()
+  //     .type('1')
   //   cy.get('@onPinChange').should('have.been.calledOnce')
   // })
+
+  it('Deve conter ', () => {
+    cy.get('[data-testid = "pinToken-0"]')
+  })
 })
