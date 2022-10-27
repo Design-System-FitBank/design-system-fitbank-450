706be85d14ebcd6dfa3dba9c73726ed74643fc8e
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-10-27 18:05:20 -0300

    test: finalização dos testes do componente Token

--- src/components/PinToken/PinToken.cy.tsx
+++ src/components/PinToken/PinToken.cy.tsx
@@ -12,14 +12,22 @@ describe('PinToken', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <PinToken onChange={cy.stub().as('onChange')} />
+        <PinToken onChange={cy.stub().as('onPinChange')} />
       </ThemeDSProvider>
     )
-    cy.wait(100)
+    cy.wait(500)
   })
 
   afterEach(() => {
-    cy.wait(100)
+    cy.wait(500)
+  })
+
+  it('Deve conter o componente token com um conjunto de 6 inputs horizontalmente', () => {
+    cy.get('[data-testid = "container"]')
+      .should('have.css', 'display', 'flex')
+      .and('have.css', 'flex-direction', 'row')
+      .children()
+      .should('have.length', 6)
   })
 
   it('Deve conter um container por volta do PinToken', () => {
@@ -32,7 +40,7 @@ describe('PinToken', () => {
   })
 
   it('Deve conter o PinToken default', () => {
-    cy.get('[data-testid = "pinToken-0"]')
+    cy.get('[data-testid = "pinToken-1"]')
       .should('have.css', 'width', '42px')
       .and('have.css', 'height', '42px')
       .and('have.css', 'background-color', 'rgb(255, 255, 255)')
@@ -44,30 +52,26 @@ describe('PinToken', () => {
   })
 
   it('Deve conter o estado Hover com uma aparencia da elevação do componente', () => {
-    cy.get('[data-testid="pinToken-0"]').realHover()
-    cy.get('[data-testid="pinToken-0"]').should('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
+    cy.get('[data-testid="pinToken-2"]').realHover()
+    cy.get('[data-testid="pinToken-2"]').should('have.css', 'box-shadow', 'rgba(0, 0, 0, 0.1) 2px 2px 4px 0px')
   })
 
   it('Deve conter o estado Focused quando o pinToken for clicado', () => {
-    cy.get('[data-testid="pinToken-0"]').click()
+    cy.get('[data-testid="pinToken-0"]')
     cy.get('[data-testid="pinToken-0"]').should('have.css', 'border', '1px solid rgb(1, 7, 22)')
   })
 
   it('Deve conter o estado Error quando o pinToken clicado for digitado um caractere for diferente de um numero', () => {
     const isNaNChar = faker.random.alpha()
 
-    cy.get('[data-testid="pinToken-0"]')
-      .click()
-      .type(isNaNChar)
-      .should('have.css', 'border', '1px solid rgb(234, 62, 62)')
+    cy.get('[data-testid="pinToken-0"]').type(isNaNChar).should('have.css', 'border', '1px solid rgb(234, 62, 62)')
   })
 
-  const validPin = faker.random.numeric(1)
+  const validPin = faker.datatype.number({min: 0, max: 9})
 
-  it('Deve conter o estado Filed quando o pinToken clicado for digitado um numero', () => {
+  it.only('Deve conter o estado Filed quando o pinToken clicado for digitado um numero', () => {
     cy.get('[data-testid="pinToken-0"]')
-      .click()
-      .type(validPin)
+      .type(validPin.toString())
       .should('have.css', 'border', '1px solid rgb(196, 196, 196)')
       .and('have.css', 'text-align', 'center')
       .and('have.css', 'font-size', '18px')
@@ -86,24 +90,24 @@ describe('PinToken', () => {
       .and('have.css', 'background-color', 'rgb(196, 196, 196)')
   })
 
-  const pinValues = [
-    faker.random.numeric(1),
-    faker.random.numeric(1),
-    faker.random.numeric(1),
-    faker.random.numeric(1),
-    faker.random.numeric(1),
-    faker.random.numeric(1)
-  ]
-
-  it.only('Deve a chamar a função onPinChange', () => {
-    pinValues.forEach((pin, index) => {
-      cy.get(`[data-testid="pinToken-${index}"]`).click().type(pin)
-    })
-    cy.get('@onPinChange').should('have.been.calledOnce')
+  it('Dado o primeiro elemento já preenchido, então o elemento seguinte ficará focado e pronto para preencher', () => {
+    cy.get('[data-testid = "pinToken-0"]').type(validPin.toString()).wait(100).should('not.have.focus')
+    cy.get('[data-testid = "pinToken-1"]').should('have.focus').and('have.css', 'border', '1px solid rgb(1, 7, 22)')
   })
 
-  it('Dado o primeiro elemento já preenchido, então o elemento seguinte ficará focado e pronto para preencher', () => {
-    cy.get('[data-testid = "pinToken-0"]').click().type(validPin).wait(100).should('not.have.focus')
-    cy.get('[data-testid = "pinToken-1"]').should('have.focus').and('have.css', 'border', '1px solid rgb(1, 7, 22)')
+  const pinValues = [
+    faker.datatype.number({min: 0, max: 9}),
+    faker.datatype.number({min: 0, max: 9}),
+    faker.datatype.number({min: 0, max: 9}),
+    faker.datatype.number({min: 0, max: 9}),
+    faker.datatype.number({min: 0, max: 9}),
+    faker.datatype.number({min: 0, max: 9}),
+  ]
+
+  it('Deve a chamar a função onPinChange', () => {
+    pinValues.forEach((pin, index) => {
+      cy.get(`[data-testid="pinToken-${index}"]`).type(pin.toString())
+    })
+    cy.get('@onPinChange').should('have.been.calledWithExactly', pinValues)
   })
 })
