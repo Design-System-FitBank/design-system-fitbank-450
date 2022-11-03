1a4cf84a4ed74c4d756126f48979f3e6998840f1
fit-CaioLima <caio.bezerra@fitbank.com.br>
2022-11-03 17:47:40 -0300

    feat: implementação do icone de lupa no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
+import { Icon } from "../Icon";
 import React from "react";
 import * as Styled from './styles'
 
 export const Header = () => {
     return (<Styled.Container data-testid="container">
-        <Styled.Input 
+        <Styled.Wrap data-testid="wrap">
+        <Styled.Input
             data-testid='input'
             placeholder="Pesquise"
-
         />
-        
+        <Styled.Search
+            data-testid='lupa'>
+            <Icon width={24} height={24} name= "search"/>
+        </Styled.Search>
+        </Styled.Wrap>
     </Styled.Container>)
 }
