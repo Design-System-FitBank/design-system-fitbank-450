9e1629c7a706b1749c1fab2786bff2933a34be19
cledson-leite <wesley.morais@fitbank.com.br>
2022-11-04 11:24:21 -0300

    refactor: criação de componente de tipos de dados

--- src/components/Detail/DataUser/index.tsx
+++ src/components/Detail/DataUser/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { DetailProps } from '../.'
+import { User } from '../Data'
 import { Typography } from '../../Typography'
 import * as Styled from './styles'
 
 interface DataProps {
-  dataUser: { businessUnit: string; accountName: string; uniqueIdentifier: string }
+  dataUser: User
 }
 
 export const DataUser: React.FC<DataProps> = ({ dataUser }) => {
@@ -19,7 +19,7 @@ export const DataUser: React.FC<DataProps> = ({ dataUser }) => {
       <Styled.Identifier data-testid='wrap'>
         <Styled.Label data-testid='labelIdentifier'>CPF/CNPJ:</Styled.Label>
         <Styled.UniqueIdentifier data-testid='uniqueIdentifier'>
-          <Typography variant='bodySmall'>{dataUser.uniqueIdentifier}</Typography>
+          <Typography variant='bodySmall'>{dataUser.userId}</Typography>
         </Styled.UniqueIdentifier>
       </Styled.Identifier>
     </Styled.Container>
--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -4,42 +4,41 @@ import { Icon } from '../Icon'
 import { DataUser } from './DataUser'
 import { Typography } from '../Typography'
 import { Button } from '../Button'
+import { User } from './Data'
 
 export interface DetailProps {
-  dataUser: { businessUnit: string; accountName: string; uniqueIdentifier: string }
+  dataUser: User
   onClick: () => void
 }
 
 export const Detail: React.FC<DetailProps> = ({ dataUser, onClick }) => {
   return (
-    <>
-      <Styled.Container data-testid='container'>
-        <Styled.Avatar data-testid='avatar'>
-          <Icon name='enterprise' />
-        </Styled.Avatar>
-        <DataUser data-testid='data' dataUser={dataUser} />
-        <Styled.Accordion data-testid='accordion'>
-          <Styled.Arrow data-testid='arrow'>
-            <Icon name='arrowDown' width={20} height={20}></Icon>
-          </Styled.Arrow>
-          <Styled.LabelAccordion data-testid='labelAccordion'>
-            <Typography variant='subtitle'>Ver dados da conta</Typography>
-          </Styled.LabelAccordion>
-        </Styled.Accordion>
-        <Styled.CopyData data-testid='copyData'>
-          <Button icon='share' onClick={onClick} size='small'>
-            Copiar
-          </Button>
-        </Styled.CopyData>
-        <Styled.ChangeAccount data-testid='changeAccount'>
-          <Styled.Refresh data-testid='refresh'>
-            <Icon name='refresh' width={20} height={20}></Icon>
-          </Styled.Refresh>
-          <Styled.LabelChangeAccount data-testid='labelChangeAccount'>
-            <Typography variant='subtitle'>Trocar de conta</Typography>
-          </Styled.LabelChangeAccount>
-        </Styled.ChangeAccount>
-      </Styled.Container>
-    </>
+    <Styled.Container data-testid='container'>
+      <Styled.Avatar data-testid='avatar'>
+        <Icon name='enterprise' />
+      </Styled.Avatar>
+      <DataUser data-testid='data' dataUser={dataUser} />
+      <Styled.Accordion data-testid='accordion'>
+        <Styled.Arrow data-testid='arrow'>
+          <Icon name='arrowDown' width={20} height={20}></Icon>
+        </Styled.Arrow>
+        <Styled.LabelAccordion data-testid='labelAccordion'>
+          <Typography variant='subtitle'>Ver dados da conta</Typography>
+        </Styled.LabelAccordion>
+      </Styled.Accordion>
+      <Styled.CopyData data-testid='copyData'>
+        <Button icon='share' onClick={onClick} size='small'>
+          Copiar
+        </Button>
+      </Styled.CopyData>
+      <Styled.ChangeAccount data-testid='changeAccount'>
+        <Styled.Refresh data-testid='refresh'>
+          <Icon name='refresh' width={20} height={20}></Icon>
+        </Styled.Refresh>
+        <Styled.LabelChangeAccount data-testid='labelChangeAccount'>
+          <Typography variant='subtitle'>Trocar de conta</Typography>
+        </Styled.LabelChangeAccount>
+      </Styled.ChangeAccount>
+    </Styled.Container>
   )
 }
