b565a8d8900b65b3ef482f16ec637d3f9bffc8f3
cledson-leite <wesley.morais@fitbank.com.br>
2022-11-04 11:24:21 -0300

    refactor: criação de componente de tipos de dados

---
+++ src/components/Detail/Data.tsx
@@ -0,0 +1,12 @@
+export interface User {
+  businessUnit: string
+  accountName: string
+  userId: string
+}
+
+export interface DataAccount {
+  bank: string
+  bankBranch: string
+  bankAccount: string
+  accountId: string
+}
