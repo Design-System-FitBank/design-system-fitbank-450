4a0bcd1215e1b52a27870609a9092c184dcb199a
fit-CaioLima <caio.bezerra@fitbank.com.br>
2022-11-04 18:29:06 -0300

    feat: implementação do avatar no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,40 @@
-import { Icon } from "../Icon";
-import React from "react";
+import { Icon } from '../Icon'
+import React, { useState } from 'react'
 import * as Styled from './styles'
 
-export const Header = () => {
-    return (<Styled.Container data-testid="container">
-        <Styled.Wrap data-testid="wrap">
+interface HeaderProps {
+  /**
+   * Função que capta os valores digitados no campo de entrada de texto
+   */
+  onSearchChange: (text: string) => void
+}
+
+export const Header = ({ onSearchChange }: HeaderProps) => {
+  const [textValue, setTextValue] = useState<string>()
+
+  const handleChange = (event: any) => {
+    let textEntry: string = event.target.value
+    setTextValue(textEntry)
+    onSearchChange(textEntry)
+  }
+  return (
+    <Styled.Container data-testid='container'>
+      <Styled.Wrap data-testid='wrap'>
         <Styled.Input
-            data-testid='input'
-            placeholder="Pesquise"
+          data-testid='input'
+          value={textValue}
+          onChange={event => handleChange(event)}
+          placeholder='Pesquise'
         />
-        <Styled.Search
-            data-testid='lupa'>
-            <Icon width={24} height={24} name= "search"/>
+        <Styled.Search data-testid='lupa'>
+          <Icon width={24} height={24} name='search' />
         </Styled.Search>
-        </Styled.Wrap>
-    </Styled.Container>)
+      </Styled.Wrap>
+      <Styled.Span>
+        <Styled.Avatar data-testid='avatar'>
+          <Icon width={35} height={35} name='user' />
+        </Styled.Avatar>
+      </Styled.Span>
+    </Styled.Container>
+  )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,40 +1,52 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
-display: flex;
-height: ${({ theme }) => theme.sizes['64px']};
- border: 1px solid;
- width : 100%;
- background-color: ${({ theme }) => theme.colors.primary};
- align-items: center;
- padding-left: ${({ theme }) => theme.sizes['12px']};
- `
+  width: 100%;
+  display: flex;
+  border: 1px solid;
+  align-items: center;
+  height: ${({ theme }) => theme.sizes['64px']};
+  padding-left: ${({ theme }) => theme.sizes['12px']};
+  background-color: ${({ theme }) => theme.colors.primary};
+  justify-content: space-between;
+`
 export const Input = styled.input`
-    height: ${({ theme }) => theme.sizes['38px']};
-    width: ${({ theme }) => theme.sizes['410px']}; 
-    border-radius: ${({ theme }) => theme.sizes['10px']};
-    padding-left: ${({ theme }) => theme.sizes['28px']};
-    font-style: italic;
-    font-weight: ${({ theme }) => theme.font.weight.tiny};
-    font-size: ${({ theme }) => theme.sizes['12px']};
-    line-height: ${({ theme }) => theme.sizes['14px']};
+  overflow-x: hidden;
+  height: ${({ theme }) => theme.sizes['38px']};
+  width: ${({ theme }) => theme.sizes['410px']};
+
+  font-style: italic;
+  font-size: ${({ theme }) => theme.sizes['12px']};
+  line-height: ${({ theme }) => theme.sizes['14px']};
+  font-weight: ${({ theme }) => theme.font.weight.tiny};
+
+  border-color: rgba(255, 255, 255, 0.1);
+  background-color: rgba(255, 255, 255, 0.1);
+  color: ${({ theme }) => theme.colors.light};
+
+  padding-left: ${({ theme }) => theme.sizes['28px']};
+  border-radius: ${({ theme }) => theme.sizes['10px']};
+  padding-right: ${({ theme }) => theme.sizes['46px']};
+
+  ::placeholder {
     color: ${({ theme }) => theme.colors.light};
-    background-color: rgba(255, 255, 255, 0.1);
-    border-color: rgba(255, 255, 255, 0.1);
-    ::placeholder {
-        color: ${({ theme }) => theme.colors.light};
-      }
- `
+  }
+`
 export const Search = styled.div`
-    color: ${({ theme }) => theme.colors.light};
-    position: absolute;
-    right: 20px;
-    top: 8px;
-    cursor: pointer;
-    max-width: 100px;
- `
+  top: 8px;
+  cursor: pointer;
+  position: absolute;
+  right: ${({ theme }) => theme.sizes['20px']};
+  color: ${({ theme }) => theme.colors.light};
+`
 export const Wrap = styled.div`
-    display: flex;
-    position: relative;
-    align-items: center;  
- `
+  display: flex;
+  position: relative;
+  align-items: center;
+`
+export const Avatar = styled.div`
+  color: ${({ theme }) => theme.colors.light};
+`
+export const Span = styled.span`
+  padding-right: ${({ theme }) => theme.sizes['144px']};
+`
