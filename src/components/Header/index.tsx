4722dd2814853a8a73a001711f85811a9788ed96
fit-CaioLima <caio.bezerra@fitbank.com.br>
2022-11-07 18:11:24 -0300

    feat: implementação dos componetes nome do usuario e o icone de home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,25 @@
 import { Icon } from '../Icon'
-import React, { useState } from 'react'
+import React, { Children, useState } from 'react'
 import * as Styled from './styles'
+import { Typography } from '../Typography'
 
 interface HeaderProps {
   /**
    * Função que capta os valores digitados no campo de entrada de texto
    */
-  onSearchChange: (text: string) => void
+  onSearch: (text: string) => void
+  /**
+   * Variável que ira retornar o nome do Usuario.
+   */
+  accountName?: string
 }
 
-export const Header = ({ onSearchChange }: HeaderProps) => {
+export const Header = ({ onSearch, accountName = 'Usuario' }: HeaderProps) => {
   const [textValue, setTextValue] = useState<string>()
 
   const handleChange = (event: any) => {
     let textEntry: string = event.target.value
     setTextValue(textEntry)
-    onSearchChange(textEntry)
   }
   return (
     <Styled.Container data-testid='container'>
@@ -26,7 +30,7 @@ export const Header = ({ onSearchChange }: HeaderProps) => {
           onChange={event => handleChange(event)}
           placeholder='Pesquise'
         />
-        <Styled.Search data-testid='lupa'>
+        <Styled.Search data-testid='lupa' onClick={() => onSearch(textValue!)}>
           <Icon width={24} height={24} name='search' />
         </Styled.Search>
       </Styled.Wrap>
@@ -34,6 +38,13 @@ export const Header = ({ onSearchChange }: HeaderProps) => {
         <Styled.Avatar data-testid='avatar'>
           <Icon width={35} height={35} name='user' />
         </Styled.Avatar>
+        <Styled.Usuario data-testid='usuario'>
+          <Typography variant='body'>Olá,</Typography>
+          <Typography variant='body'>{accountName ? accountName : 'usuario'}</Typography>
+        </Styled.Usuario>
+        <Styled.Home data-testid='home'>
+          <Icon width={22} height={22} name='options' />
+        </Styled.Home>
       </Styled.Span>
     </Styled.Container>
   )
