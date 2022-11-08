fa41235cd4e47b2bf73e741c70875b3e22d64f21
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-11-08 17:23:45 -0300

    test: implementação dos testes adicionais do sidebar

--- src/components/Sidebar/Sidebar.cy.tsx
+++ src/components/Sidebar/Sidebar.cy.tsx
@@ -5,16 +5,16 @@ import 'cypress-real-events/support'
 import { mount } from 'cypress/react18'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 
-import { Sidebar, NavButtonListProps } from '.'
+import { Sidebar, NavButtonListProps } from './'
 
 describe('Sidebar', () => {
   const navButtonList: NavButtonListProps[] = [
-    { label: 'Transferencia', icon: 'moneyTransfer', onClick: () => { cy.stub().as('') }},
-    { label: 'Area Pix', icon: 'pix', onClick: () => { cy.stub().as('') }},
-    { label: 'Pagamento', icon: 'payment', onClick: () => { cy.stub().as('') }},
+    { label: 'Pix', icon: 'pix', onClick: () => { cy.stub().as('') }},
     { label: 'Recarga', icon: 'topup', onClick: () => { cy.stub().as('') }},
+    { label: 'Cartões', icon: 'cardMulti', onClick: () => { cy.stub().as('') }},
+    { label: 'Pagamento', icon: 'payment', onClick: () => { cy.stub().as('') }},
     { label: 'Agendamento', icon: 'calendar', onClick: () => { cy.stub().as('') }},
-    { label: 'Cartões', icon: 'cardMulti', onClick: () => { cy.stub().as('') }}
+    { label: 'Transferencia', icon: 'moneyTransfer', onClick: () => { cy.stub().as('') }}
   ]
 
   beforeEach(() => {
@@ -75,9 +75,23 @@ describe('Sidebar', () => {
 
   it('Deve conter o componente Sidebar com o fechado', () => {
     cy.get('[data-testid = "close-button-container"]').click()
+    cy.get('[data-testid = "sidebar-container"]')
+      .should('have.css', 'width', '62px')
+    cy.get('[data-testid = "logo-container"]')
+      .should('have.css', 'width', '56px')
+      .and('have.css', 'margin', '20px 4px')
+    cy.get('[data-testid = "close-button-container"]')
+      .should('have.css', 'justify-content', 'center')
+    cy.get('[data-testid = "user-detail"]')
+      .should('have.css', 'height', '80px')
+    cy.get('[data-testid = "nav-button-grid"]')
+      .should('have.css', 'flex-direction', 'column')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'align-items', 'center')
   })
 
   it('Deve conter o componente Sidebar aberto depois de ter sido fechado', () => {
+    cy.get('[data-testid = "close-button-container"]').click()
     cy.get('[data-testid = "close-button-container"]').wait(700).click()
   })
 })
