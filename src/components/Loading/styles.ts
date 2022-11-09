9df5bd158d30073f5155b7d35d667431f7e2a9ee
Gabriel Rodrigues <gabriel.moura@fitbank.com.br>
2022-11-09 13:21:47 -0300

    feat: implementação do componente Loading

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,6 +1,28 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const isRotating = keyframes`
+  to {
+    transform: rotate(1turn);
+  }
+`
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  transform: scale(1);
+`
+export const Loader = styled.div`
+  animation-name: isRotating;
+
+  width: 50px;
+  height: 50px;
+
+  border: 6px solid #e5e5e5;
+  border-top-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 50%;
 `
