9ad675e877ca94a305884c20375aace776af0395
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-11-16 12:26:05 -0300

    refactor: ajuste do labe e children no teste

--- src/components/Accordion/Accordion.cy.tsx
+++ src/components/Accordion/Accordion.cy.tsx
@@ -1,28 +1,16 @@
-import { mount } from 'cypress/react18'
 import React from 'react'
+import { mount } from 'cypress/react18'
+import {faker} from '@faker-js/faker'
 import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
 import { Accordion } from '.'
 
 describe('Accordion component', () => {
+  const label = faker.lorem.sentence(2)
   const children = () => {
+    const list = Array(10).fill('dados')
     return (
       <div style={{ height: 320 }}>
-        dados
-        <br />
-        dados
-        <br />
-        dados
-        <br />
-        dados
-        <br />
-        dados
-        <br />
-        dados
-        <br />
-        dados
-        <br />
-        dados
-        <br />{' '}
+        {list.map((item: string) => <div>item</div>)}
       </div>
     )
   }
@@ -31,7 +19,7 @@ describe('Accordion component', () => {
     mount(
       <ThemeDSProvider theme={Theme}>
         <GlobalStyles />
-        <Accordion children={children()} label='Ver dados da conta' />
+        <Accordion children={children()} label={label} />
       </ThemeDSProvider>
     )
   })
@@ -42,7 +30,7 @@ describe('Accordion component', () => {
     cy.get('[data-testid="accordion"]').should('have.css', 'cursor', 'pointer').and('have.css', 'height', '40px')
     cy.get('[data-testid="arrow"]').should('have.css', 'padding-top', '4px').and('have.css', 'color', 'rgb(50, 55, 81)')
     cy.get('[data-testid="labelAccordion"]')
-      .should('have.text', 'Ver dados da conta')
+      .should('have.text', label)
       .and('have.css', 'color', 'rgb(50, 55, 81)')
       .and('have.css', 'padding-left', '12px')
   })
@@ -62,6 +50,6 @@ describe('Accordion component', () => {
     cy.get('[ data-testid="button"]').click()
     cy.get('[data-testid="arrow"]').get('[data-testid="arrowUp"]').should('not.exist')
     cy.get('[data-testid="arrow"]').get('[data-testid="arrowDown"]').should('exist')
-    cy.get('[data-testid="labelAccordion"]').should('have.text', 'Ver dados da conta')
+    cy.get('[data-testid="labelAccordion"]').should('have.text', label)
   })
 })
