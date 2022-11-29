cd10459a329cb62577d1d9f1a76fe7cacd9361c6
Fit-Jonas <jonas.leitao@fitbank.com.br>
2022-11-29 10:20:20 -0300

    feat: criado a parte de teste

---
+++ src/components/Card/Card.cy.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import 'cypress-real-events/support'
+import { Card } from './'
+import { GlobalStyles, Theme, ThemeDSProvider } from '../../theme'
+
+describe('Card Default', () => {
+  beforeEach(() => cy.wait(1000))
+  afterEach(() => cy.wait(1000))
+
+  it('Deve ser o componente Default quando não for informado o type', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Card elevation={false}></Card>
+      </ThemeDSProvider>
+    )
+  })
+  it('É setado o tamanho e a cor do componente', () => {
+    cy.mount(
+      <ThemeDSProvider theme={Theme}>
+        <GlobalStyles />
+        <Card width={200} height={300} color='dark' size='setSize'></Card>
+      </ThemeDSProvider>
+    )
+  })
+})
