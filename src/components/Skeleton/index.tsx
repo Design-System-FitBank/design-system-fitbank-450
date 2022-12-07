02a0177e2749e2a3afcd314a317ec334e229e79a
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2022-12-07 17:59:28 -0300

    refactor: adição das descrições das propriedades

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 import * as Styled from './styles';
 
 interface SkeletonProps {
+  /**
+   * altura obrigatória do componente
+   */
   height: number
+  /**
+   * largura obrigatória do componente
+   */
   width: number
+  /**
+   * radio da curvatura das pontas do componente
+   */
   radius: number
 }
 
