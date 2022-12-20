d59cc864ca2291abb10b42602b92225147ba3b52
lucas nascimento <lucas.nascimento@fitbank.com.br>
2022-12-20 14:34:06 -0300

    fix: reenvio do comoponente loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '../Typography'
+import React from 'react'
+
+import * as Styled from './styles'
+
+interface LoadingProps {
+  text?: string
+}
+
+export const Loading: React.FC<LoadingProps> = ({ text }) => {
+  return (
+    <Styled.Container data-testid='container'>
+      <Styled.Loader data-testid='loader-container' />
+      {text && <Typography variant='h6'>{text}</Typography>}
+    </Styled.Container>
+  )
+}
