149b825f2c5093083e3338bbe24ce249eaa6213d
fitbank-cledson-leite <cledson-leite@fitbank.com.br>
2023-01-03 13:57:36 -0300

    fix: ajuste de tema

--- src/components/Modal/components/FooterModal/styles.ts
+++ src/components/Modal/components/FooterModal/styles.ts
@@ -4,7 +4,7 @@ export const FooterModal = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.colors.greyLight};
+  background-color: ${({ theme }) => theme.colors.shadow};
   height: ${({ theme }) => theme.sizes['76px']};
   border-radius: 0 0 ${({ theme }) => theme.sizes['16px']} ${({ theme }) => theme.sizes['16px']};
 `
